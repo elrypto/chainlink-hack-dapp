b8920dff6b241ce6bd4d7e5e71ec6e8dfcf5c844
lrypto <lloydrebello@gmail.com>
2019-10-30 18:50:35 -0400

    fixed missing files for project copy

--- src/components/StatusAndButtons.tsx
+++ src/components/StatusAndButtons.tsx
@@ -114,7 +114,17 @@ export default function StatusAndButtons() {
             <div>Loom Network</div>
           </div>
           <div className="boxBody seeMe">
-            <div className="boxBodyStatus">l:</div>
+            <div className="boxBodyStatus">
+              <div>
+                token balance:
+              </div>
+              <div>
+                gateway balance:
+              </div>
+              <div>
+                data field:
+              </div>
+            </div>
             <div className="boxBodybuttons">
               <Button
                 type="dashed"
